dc23d9f16b3d812dc954cdf97201d06994ee87db
Leon Mohr <leon-mohr@hotmail.com>
2022-11-23 12:46:32 +0100

    added some trivia

--- init_trivia.sql
+++ init_trivia.sql
@@ -13,3 +13,6 @@ CREATE TABLE trivia_datenbank(
 
 INSERT INTO trivia_datenbank(trivia) VALUES("Kraken haben neun Gehirne und drei Herzen");
 INSERT INTO trivia_datenbank(trivia) VALUES("Napoleon Bonaparte hat in 81 Schlachten nur 11 verloren");
+INSERT INTO trivia_datenbank(trivia) VALUES("Bevor Nintendo sich zu Videospielen begeben hat, haben Sie Spielkarten verkauft");
+INSERT INTO trivia_datenbank(trivia) VALUES("Der Urkontinent der Erde heißt Pangäa");
+INSERT INTO trivia_datenbank(trivia) VALUES("In Microsoft Windows kannst du keinem Ordner den Namen con geben");
