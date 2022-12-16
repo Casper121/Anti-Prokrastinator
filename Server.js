5391c9fdcd163c22690516fc381454b53103ef89
Gandeon <leon-mohr@hotmail.com>
2022-12-16 13:54:04 +0100

    fixed chicken_counter and cleaned up a bit

--- Server.js
+++ Server.js
@@ -16,10 +16,15 @@ app.use(
   })
 );
 
+
+//Pop-Ups
 const alert = require("alert");
 
+//Verschlüsselung der Passwörter
 const bcrypt = require("bcrypt");
 
+
+//Session-Variablen
 const session = require('express-session');
 app.use(session({
     secret: 'example',
@@ -49,9 +54,6 @@ app.use(express.static(__dirname + "/images"));
 app.use(express.static(__dirname + "/html"));
 app.use(express.static(__dirname + "/views"));
 
-app.get("/Log-On",function(req,res){
-    res.sendFile(__dirname + "/views/logon.html");
-});
 
 //Die benötigten Seiten
 app.get("/Start",function(req,res){
@@ -66,12 +68,18 @@ app.get("/Fehlschlag",function(req,res){
     res.sendFile(__dirname + "/views/t1_p5_taskFailed.html");
 });
 
+app.get("/Log-On",function(req,res){
+    res.sendFile(__dirname + "/views/logon.html");
+});
+
+
 
 //Datenbanken mit sqlite3 noch serstellen, sonst funktionieren sie nicht!
 
 //Datenbanken & sqlite3
 const sqlite3 = require("sqlite3").verbose();
 
+//Datenbank für Trivia
 let db_trivia = new sqlite3.Database("trivia.db",(err)=>{
     if(err){
         return console.log(err.message);
@@ -81,15 +89,20 @@ let db_trivia = new sqlite3.Database("trivia.db",(err)=>{
 
 /*
 db_tasks wird hier im Code des Servers bei Sachen wie "db_tasks.run() oder db_tasks.all() verwendet"
-In den sql-Aufrufen wird dann aber die tasks_datenbank angesprochen, die in der sql-Datei erstellt wurde
+
+
+In SQL-Aufrufen sind dann user_datenbank oder tasks_datenbank zu verwenden
 
 */
+
+//Datenbank für Aufgaben
 let db_tasks = new sqlite3.Database("tasks.db",(err)=>{
     if(err){
         return console.log(err.message);
     }
     console.log("Connected to tasks database");
 });
+
 
 
 //Task and Time in Tier 1 setzten.
@@ -126,6 +139,9 @@ db_trivia.all(
     });
     }
 )
+
+
+
 //Anmeldung, falls user bereits besteht
 app.post("/logon",function(req,res){
 
@@ -155,6 +171,8 @@ app.post("/logon",function(req,res){
 
 });
 
+
+//Benutzer abmelden
 app.post("/logoff", function (req, res) {
     delete req.session.sessionValue;
     console.log(req.session.sessionValue != 1);
@@ -205,41 +223,64 @@ app.post("/sign_up",function(req,res){
 //Aufgabe wurde geschafft!
 app.post("/ergebnis_ja",function(req,res){
 
-    //Die zuletzt eingetragene Aufgabe wird aufgerufen und abgeändert, wenn sie gemacht wurde
-
-    //Hier das is_done updaten, damit es in task_done angezeigt werden kann
-    //Dazu muss der neueste Eintrag abgerufen werden
-    var temp_chicken_status = 0;
+    //Temporäre Variablen erstellen
+    var temp_chicken_status;
     var temp_username = req.session.sessionValue;
     var chicken_image_path = "";
 
     console.log(temp_username);
      
+    //Die zuletzt eingetragene Aufgabe wird aufgerufen und abgeändert, wenn sie gemacht wurde
+    //Hier das is_done updaten, damit es in task_done angezeigt werden kann
+    //Dazu muss der neueste Eintrag abgerufen werden
     db_tasks.run(
 
         `UPDATE tasks_datenbank SET is_done = "ja" WHERE id = (SELECT MAX(id) FROM tasks_datenbank) `
 
-        );
+    );
 
    
 
 
     db_tasks.all(
+
         `SELECT * FROM user_datenbank WHERE username_data = '${temp_username}'`,
         function(err,row){
+
+            //temporäre Variable nimmt aktuellen Wert in der Datenbank an
+            temp_chicken_status = row[0].chicken_status;
+            console.log("temporärer chicken_status vor: " + temp_chicken_status);
+
+            //Sofern der counter unter 5 ist, wird inkrementiert
             if(row[0].chicken_status <5){
 
                 console.log("Chicken Status vor Inkrement:" + row[0].chicken_status);
 
+                //Chicken_status inkrementieren
                 db_tasks.run(
                     `UPDATE user_datenbank SET chicken_status = chicken_status + 1 WHERE username_data = '${temp_username}'`
                     
                 )
+                
+                //Debug; Datenbankeintrag wird nur verzögert inkrementiert, daher funktionierte es vorher nicht
+                console.log("Chicken Status nach Inkrement:" + row[0].chicken_status);
+
+                //Stattdessen wird temporärer Wert benutzt
+                temp_chicken_status += 1;
+                console.log("temporärer chicken_status nach: " + temp_chicken_status);
+
+
             }
-
-            temp_chicken_status=row[0].chicken_status;
             
+        }
+    )
 
+    db_tasks.all(
+        `SELECT * FROM user_datenbank WHERE username_data = '${temp_username}'`,
+        function(err,row){
+            
+            
+            //Ei wird ausgesucht
             switch(temp_chicken_status){
                 case 1:
 
@@ -267,17 +308,15 @@ app.post("/ergebnis_ja",function(req,res){
                 break;
             }
 
-            console.log("Chicken Status nach Inkrement:" + row[0].chicken_status);
-
-            console.log(temp_chicken_status);
             console.log(chicken_image_path);
 
             res.render("t1_p4_taskDone",{image:chicken_image_path});
-            
         }
     )
 
-        //Hier wird der Eintrag abgeschickt
+
+
+        //Hier wird der Eintrag abgeschickt, falls er im erfolgreichem screen gezeigt werden soll
     /*
     db_tasks.all(
         `SELECT * FROM tasks_datenbank WHERE id = (SELECT MAX(id) FROM tasks_datenbank) `,
@@ -287,37 +326,62 @@ app.post("/ergebnis_ja",function(req,res){
             res.render("t1_p4_taskDone",{task_done: task_success});
         }
     );
-    */
-
+    
+*/
 });
 
+
+//Wenn die Aufgabe nicht geschafft wurde
 app.post("/ergebnis_nein",function(req,res){
 
-    //Die zuletzt eingetragene Aufgabe wird aufgerufen und abgeändert, wenn sie gemacht wurde
-
-    //Hier das is_done updaten, damit es in task_done angezeigt werden kann
-    //Dazu muss der neueste Eintrag abgerufen werden
-    var temp_chicken_status = 0;
+    //Temporäre Variablen erstellen
+    var temp_chicken_status;
     var temp_username = req.session.sessionValue;
     var chicken_image_path = "";
 
+    //Debugging, um korrekten Benutzer zu verifizieren
     console.log("Derzeitiger user: " + temp_username);
     
+    //Chicken_status wird um 1 dekrementiert, sofern der counter größer 1 ist
+    db_tasks.all(
+        `SELECT * FROM user_datenbank WHERE username_data = '${temp_username}'`,
+        function(err,row){
 
-    db_tasks.run(
-        `UPDATE user_datenbank SET chicken_status = chicken_status -1  WHERE username_data = '${temp_username}'`
-        
+            temp_chicken_status = row[0].chicken_status;
+            //Debugging
+            console.log("temporärer chicken_status vor: " + temp_chicken_status);
+
+
+            if(row[0].chicken_status > 1 ){
+
+                console.log("Chicken Status vor Dekrement:" + row[0].chicken_status);
+
+                db_tasks.run(
+                    `UPDATE user_datenbank SET chicken_status = chicken_status -1  WHERE username_data = '${temp_username}'`
+                    
+                )
+                    
+                
+                console.log("Chicken Status nach Dekrement: " + row[0].chicken_status);
+                temp_chicken_status -= 1;
+                console.log("temporärer chicken_status nach: " + temp_chicken_status);
+
+            }
+
+           
+            
+        }
     )
 
 
+
+    
+    //Bild wird anhand des chicken_status ausgewählt
     db_tasks.all(
 
         `SELECT * FROM user_datenbank WHERE username_data = '${temp_username}'`,
         function(err,row){
            
-            temp_chicken_status=row[0].chicken_status;
-            
-
             switch(temp_chicken_status){
                 case 1:
 
@@ -343,11 +407,11 @@ app.post("/ergebnis_nein",function(req,res){
                 chicken_image_path = "Chicken_Adult.png";
                 break;
             }
-            console.log(temp_chicken_status);
+            
+
             console.log(chicken_image_path);
 
 
-            console.log("Chicken Status nach Dekrement: " + row[0].chicken_status);
             res.render("t1_p5_taskFailed",{image:chicken_image_path});
             
         }
@@ -357,7 +421,7 @@ app.post("/ergebnis_nein",function(req,res){
 
 });
 
-
+//Liste der bisherigen Aufgaben wird abgerufen
 app.post("/task_list",function(req,res){
     db_tasks.all(
         `SELECT * FROM tasks_datenbank`,function(err,rows){
